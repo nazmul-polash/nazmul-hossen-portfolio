32db117ab90b86ba9315560e2251b769bcf82771
Nazmul Polash <mdpoash01980@gmail.com>
2025-08-25 01:45:58 +0600

    hero and navabar update

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -9,13 +9,12 @@ export default function HeroImage() {
       <div className='absolute right-0 top-0 h-[550px] w-full overflow-hidden rounded-bl-full 
       rounded-br-full border-r-[10px] border-blue-500 bg-gray-700 md:h-[600px] md:w-[400px] '>
 
-         <AnimatedIcon Icon={FaReact } className="left-10 top-25" iconClassName="text-blue-500"/>   
-         <AnimatedIcon Icon={RiTailwindCssFill  } className="right-10 top-25" iconClassName="text-blue-600"/>   
-         <AnimatedIcon Icon={FaDatabase } className="left-10 bottom-25" iconClassName="text-blue-300"/>   
-         <AnimatedIcon Icon={FaLaravel  } className="right-10  bottom-25" iconClassName="text-red-500"/>   
-
-
-         <img alt="Profile" className="ablsolute bottom-0 left-1/2 w-[650px]" 
+         <AnimatedIcon Icon={FaReact } className="left-10 top-35" iconClassName="text-blue-500"/>   
+         <AnimatedIcon Icon={RiTailwindCssFill  } className="right-10 top-35" iconClassName="text-blue-600"/>   
+         <AnimatedIcon Icon={FaDatabase } className="left-10 bottom-35" iconClassName="text-blue-300"/>   
+         <AnimatedIcon Icon={FaLaravel} className="right-10  bottom-35" iconClassName="text-red-500" />
+         
+         <img alt="Profile" className="ablsolute mt-25 bottom-0 left-1/2 w-[650px]" 
             src="https://png.pngtree.com/png-vector/20231018/ourmid/pngtree-business-portfolio-3d-character-illustration-png-image_10204176.png"/>
       </div>
    )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router'
 import ToggleTheme from './ToggleTheme'
+import { HiMenu, HiOutlineX } from "react-icons/hi";
 
 export default function Navbar() {
+   const [isMenuOpen, setisMenuOpen] = useState();
+   const toggleManue = () => {
+      setisMenuOpen(!isMenuOpen);
+   }
    return (
       <>
          <header className='fixed top-0 z-50 w-full px-4 py-4'>
             <div className="container flex justify-between items-center rounded-full border-2 border-white/5 p-2
                bg-white/5 shadow-xl backdrop-blur mx-auto">
-               
                <div className="navbar-start">
-                  <div className="dropdown">
-                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                     </div>
-                     <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                           <a>Parent</a>
-                           <ul className="p-2">
-                              <li><a>Submenu 1</a></li>
-                              <li><a>Submenu 2</a></li>
-                           </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                     </ul>
+                  <div className="btn btn-ghost lg:hidden" onClick={toggleManue} area-label="Toggle Menu"
+                     aria-expanded={isMenuOpen}>
+                     {
+                        isMenuOpen ? <HiOutlineX className='text-3xl' /> : <HiMenu className='text-3xl' />
+                     }
                   </div>
+
+                  {isMenuOpen && (
+                     <div className="fixed top-16 left-0 w-full bg-gray-800/95 backdrop-blur-lg p-4 
+                        lg:hidden text-white transition-transform transform duration-300
+                        ease-in-out opacity-100 scale-y-100">
+                        <ul className="menu menu-vertical px-1 text-xl space-y-3">
+                           <li><Link to="/">About</Link></li>
+                           <li><Link to="/">Experience</Link></li>
+                           <li><Link to="/">Education</Link></li>
+                           <li><Link to="/">Skills</Link></li>
+                           <li><Link to="/">Projects</Link></li>
+                           <li><Link to="/">Contact</Link></li>
+                        </ul>
+                     </div>
+                  )}
+
                   <a className="text-2xl hidden lg:block ml-4">NH</a>
                </div>
 
@@ -52,7 +58,6 @@ export default function Navbar() {
                </div>
             </div>
          </header>
-
       </>
    )
 }
